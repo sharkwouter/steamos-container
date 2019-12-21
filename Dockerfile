b11bef3053b703ac361a5402297b5ee4b4b51aba
Wouter Wijsman <wwijsman@live.nl>
2019-12-21 18:13:50 +0100

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+from ubuntu:latest
+
+ENV NAME SteamOS
+ENV RELEASE brewmaster
+ENV URL http://repo.steampowered.com/download/brewmaster/2.195/SteamOSDVD.iso
+ENV CHECKSUM 1622cce5703e079218e7dea4bbe796c4
+ENV VERSION 2.195
+
+# Install dependencies
+RUN apt-get update && \
+ DEBIAN_FRONTEND=noninteractive \
+ apt-get install -y wget p7zip-full debootstrap fakeroot fakechroot
+
+# Download and extract the SteamOS ISO and clean up
+RUN echo $CHECKSUM $NAME.iso > checksum  && wget -O $NAME.iso $URL && md5sum -c checksum && 7z x $NAME.iso -o$NAME && rm -rf $NAME/\[BOOT\] && rm $NAME.iso checksum
+
+RUN mkdir output
+COPY build.sh build.sh
+COPY brewmaster brewmaster
+CMD ./build.sh $RELEASE $NAME output
---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+RELEASE="${1}"
+BASE="${PWD}/${2}"
+CHROOTPATH="${PWD}/chroot"
+SCRIPT="${PWD}/${RELEASE}"
+OUTPUT="${PWD}/${3}.tar.gz"
+
+if [ ! -d ${BASE} ]; then
+	echo "Error: ${BASE} directory not found, run gen.sh first"
+fi
+base_include="${BASE}/.disk/base_include"
+base_exclude="${BASE}/.disk/base_exclude"
+
+components=""
+for component in `cat ${BASE}/.disk/base_components`; do
+	if [ "${component}" == "main" ]; then
+		components="${component}"
+	else
+		components="${components},${component}"
+	fi
+done
+
+includes=""
+if [ -f ${base_include} ]; then
+	for include in `cat ${base_include}`; do
+		includes="${includes},${include}"
+		if [ "${include}" == "$(head -1 ${base_include})" ]; then
+			includes="--include=${include}"
+		else
+			includes="${includes},${include}"
+		fi
+	done
+fi
+
+excludes=""
+if [ -f ${base_exclude} ]; then
+	for exclude in `cat ${base_exclude}`; do
+		if [ "${exclude}" == "$(head -1 ${base_exclude})" ]; then
+			excludes="--exclude=${exclude}"
+		else
+			excludes="${excludes},${exclude}"
+		fi
+	done
+fi
+
+fakechroot debootstrap --variant=fakechroot --components=${components} --resolve-deps ${includes} ${excludes} --no-check-gpg ${RELEASE} ${CHROOTPATH} file://${BASE} ${SCRIPT} && \
+tar -C ${CHROOTPATH} -c . -f ${OUTPUT}
